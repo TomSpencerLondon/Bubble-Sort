d1c6069b006fdfe9d5d75e9072e69a3e30a18895
Tom Spencer <thomas.spencer@codurance.com>
2022-11-24 11:26:44 +0000

    Refactor to use for loop for first and second element swapping

--- src/test/java/com/tomspencerlondon/SortTest.java
+++ src/test/java/com/tomspencerlondon/SortTest.java
@@ -16,18 +16,22 @@ public class SortTest {
     assertThat(sort(asList(2, 1))).isSorted();
     assertThat(sort(asList(1, 2, 3))).isSorted();
     assertThat(sort(asList(2, 1, 3))).isSorted();
+    assertThat(sort(asList(1, 3, 2))).isSorted();
   }
 
   private List<Integer> sort(List<Integer> list) {
     if (list.size() > 1) {
-      if (list.get(0) > list.get(1)) {
-        int first = list.get(0);
-        int second = list.get(1);
+      for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
 
-        list.set(0, second);
-        list.set(1, first);
+        int secondIndex = firstIndex + 1;
+        if (list.get(firstIndex) > list.get(secondIndex)) {
+          int first = list.get(firstIndex);
+          int second = list.get(secondIndex);
+
+          list.set(firstIndex, second);
+          list.set(secondIndex, first);
+        }
       }
-      return list;
     }
 
     return list;
