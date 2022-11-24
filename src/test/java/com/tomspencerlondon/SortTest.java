08395dcca60a290cf3855c5c172ff483997ae006
Tom Spencer <thomas.spencer@codurance.com>
2022-11-24 11:14:49 +0000

    Add algorithm to sort zero and first element if first element is larger than second

--- src/test/java/com/tomspencerlondon/SortTest.java
+++ src/test/java/com/tomspencerlondon/SortTest.java
@@ -1,5 +1,6 @@
 package com.tomspencerlondon;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
@@ -9,16 +10,27 @@ public class SortTest {
 
   @Test
   void sortTest() {
-    assertThat(sort(List.of())).isSorted();
+    assertThat(sort(asList())).isSorted();
 
-    assertThat(sort(List.of(1))).isSorted();
+    assertThat(sort(asList(1))).isSorted();
 
-    assertThat(sort(List.of(1, 2))).isSorted();
+    assertThat(sort(asList(1, 2))).isSorted();
 
-    assertThat(sort(List.of(2, 1))).isSorted();
+    assertThat(sort(asList(2, 1))).isSorted();
   }
 
   private List<Integer> sort(List<Integer> list) {
+    if (list.size() > 1) {
+      if (list.get(0) > list.get(1)) {
+        int first = list.get(0);
+        int second = list.get(1);
+
+        list.set(0, second);
+        list.set(1, first);
+      }
+      return list;
+    }
+
     return list;
   }
 }
