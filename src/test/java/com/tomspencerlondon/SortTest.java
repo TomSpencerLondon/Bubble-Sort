0b2bc789213c33fb1c90e586f5afed892ff77a89
Tom Spencer <thomas.spencer@codurance.com>
2022-11-24 11:33:03 +0000

    Remove if statement

--- src/test/java/com/tomspencerlondon/SortTest.java
+++ src/test/java/com/tomspencerlondon/SortTest.java
@@ -22,17 +22,15 @@ public class SortTest {
   }
 
   private List<Integer> sort(List<Integer> list) {
-    if (list.size() > 1) {
-      for (int limit = list.size() - 1; limit > 0; limit--) {
-        for (int firstIndex = 0; firstIndex < limit; firstIndex++) {
-          int secondIndex = firstIndex + 1;
-          if (list.get(firstIndex) > list.get(secondIndex)) {
-            int first = list.get(firstIndex);
-            int second = list.get(secondIndex);
+    for (int limit = list.size() - 1; limit > 0; limit--) {
+      for (int firstIndex = 0; firstIndex < limit; firstIndex++) {
+        int secondIndex = firstIndex + 1;
+        if (list.get(firstIndex) > list.get(secondIndex)) {
+          int first = list.get(firstIndex);
+          int second = list.get(secondIndex);
 
-            list.set(firstIndex, second);
-            list.set(secondIndex, first);
-          }
+          list.set(firstIndex, second);
+          list.set(secondIndex, first);
         }
       }
     }
